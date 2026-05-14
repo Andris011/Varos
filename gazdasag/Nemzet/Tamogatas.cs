fade6626ec27fb402b2f0d886ac65d3f008dee2f
Balázs Kereszturi <kereszturi.balazs@petrik.hu>
2026-05-14 18:37:31 +0200

    allami tamogatasok , döme, dávid, pongor mark

---
+++ gazdasag/Nemzet/Tamogatas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Varos.gazdasag.Bank;
+
+namespace Varos.gazdasag.Nemzet
+{
+    internal class Tamogatas
+    {
+        private string szarmazas;
+        private int osszeg;
+        private string kinek;
+        private bool elerhetoE;
+
+        
+
+        public Tamogatas(string szarmazas, int osszeg, string kinek)
+        {
+            this.szarmazas = szarmazas;
+            this.osszeg = osszeg;
+            this.kinek = kinek;
+            this.elerhetoE = true;
+        }
+
+        public void Felvesz(Bankszamla szamla)
+        {
+            if (elerhetoE)
+            {
+                szamla.Feltoltes(osszeg);
+                elerhetoE = false;
+            }
+            
+        }
+
+        public string Szarmazas { get => szarmazas; set => szarmazas = value; }
+        public int Osszeg { get => osszeg; set => osszeg = value; }
+        public string Kinek { get => kinek; set => kinek = value; }
+    }
+}
