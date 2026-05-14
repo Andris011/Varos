c59614cc019d5b3c0f287e12307d7800a7467d54
furdiburd <github.molar695@passfwd.com>
2026-05-14 21:51:29 +0200

    karácsony esemény Heim Péter

---
+++ esemenyek/karacsony/Karacsony.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Varos.Lakossag;
+
+namespace Varos.esemenyek.karacsony
+{
+    internal class Karacsony
+    {
+        public void KaracsonyInditasa(List<Ember> lakossag)
+        {
+            Console.WriteLine("---------------Karácsony elkezdődött---------------");
+            Console.WriteLine("--------------Ho-ho-ho! Boldog Karácsonyt!--------------"); // Kringelin nem jár mellé
+            Console.WriteLine("--------------Mindenki boldogsága nőtt 2-vel--------");
+            Console.WriteLine("--------------Az ünnepi vacsora csökkentette az éhséget és szomjúságot 3-mal--------");
+
+            foreach (Ember ember in lakossag)
+            {
+                ember.BoldogsagSzint = Math.Min(10, ember.BoldogsagSzint + 2);
+                ember.EhessegSzint = Math.Max(1, ember.EhessegSzint - 3);
+                ember.SzomjSzint = Math.Max(1, ember.SzomjSzint - 3);
+            }
+
+            KaracsonyVege();
+        }
+
+        public void KaracsonyVege()
+        {
+            Console.WriteLine("A karácsony véget ért.");
+        }
+    }
+}
