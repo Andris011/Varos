d541cdb14966ff1289d7b8e8fecea23c977c73f0
Balázs Kereszturi <kereszturi.balazs@petrik.hu>
2026-05-14 18:23:25 +0200

    inflacio döme, dávid, pongor

---
+++ gazdasag/Arfolyam/Arfolyam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varos.gazdasag.Arfolyam
+{
+    internal class Arfolyam
+    {
+        private double inflacioMutato;
+
+        public Arfolyam(double inflacioMutato)
+        {
+            this.inflacioMutato = inflacioMutato;
+        }
+        private Random random = new Random(); 
+        public void HetiInflacio()
+        {
+            this.inflacioMutato = inflacioMutato * ((random.NextDouble()-0.5)/0.5)*0.23;
+        }
+
+        public double InflacioMutato { get => inflacioMutato; set => inflacioMutato = value; }
+
+
+    }
+}
