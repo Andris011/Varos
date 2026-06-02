b014c2fe9d417e9775b7488f9595f2c914aa81c4
Tamás <nguyen.van.tamas@petrik.hu>
2026-06-02 17:36:41 +0200

    Ember osztály kipofozva: - felesleges adattagok törölve - metódusok kezdetleges verziói létrehozva (Nguyen Van Tamás)

--- Lakossag/Ember.cs
+++ Lakossag/Ember.cs
@@ -12,66 +12,90 @@ namespace Varos.Lakossag
 {
     internal class Ember
     {
-        private int emberID;
         private string nev;
-        private int eletKor;
+        private double eletKor;
         private string nem;
         private ILakohaz lakohazID;
-        private string munka;
-        private List<string> tulajdon;
-        private int penz;
-        private string statusz; //nagykoru, kiskoru, nyugdijas
-        private int boldogsagSzint; //1-10
-        private int ehessegSzint;// 1-10
-        private int szomjSzint; //1-10
-        private int fizikum; //1-10
         private bool hazasE;
+        private Ember hazastars;
         private bool varandosE;
-        private int egeszsegSzint; //1-100
-        private int veralkoholSzint; // 1-100 7 felett kampo, 3 felett korhaz
+        private int varandossagSzamlalo;
+        private bool halottE;
         private Bankszamla bankszamla;
 
-        public Ember(int emberID, string nev, int eletKor, string nem, ILakohaz lakohazID, string munka, List<string> tulajdon, int penz, string statusz, int boldogsagSzint, int ehessegSzint, int szomjSzint, int fizikum, bool hazasE, bool varandosE, int egeszsegSzint, int veralkoholSzint, Bankszamla bankszamla)
+        public Ember(string nev, double eletKor, string nem, ILakohaz lakohazID, Bankszamla bankszamla)
         {
-            this.EmberID = emberID;
-            this.Nev = nev;
-            this.EletKor = eletKor;
-            this.Nem = nem;
-            this.LakohazID = lakohazID;
-            this.Munka = munka;
-            this.Tulajdon = tulajdon;
-            this.Penz = penz;
-            this.Statusz = statusz;
-            this.BoldogsagSzint = boldogsagSzint; // pl ide alap ertek
-            this.EhessegSzint = ehessegSzint;
-            this.SzomjSzint = szomjSzint;
-            this.Fizikum = fizikum;
-            this.HazasE = hazasE;
-            this.VarandosE = varandosE;
-            this.EgeszsegSzint = egeszsegSzint;
-            this.VeralkoholSzint = veralkoholSzint;
-            this.Bankszamla = bankszamla;
+            this.nev = nev;
+            this.eletKor = eletKor;
+            this.nem = nem;
+            this.lakohazID = lakohazID;
+            this.hazasE = false;
+            this.varandosE = false;
+            this.varandossagSzamlalo = 0;
+            this.halottE = false;
+            this.bankszamla = bankszamla;
         }
 
-        public int EmberID { get => emberID; set => emberID = value; }
         public string Nev { get => nev; set => nev = value; }
-        public int EletKor { get => eletKor; set => eletKor = value; }
+        public double EletKor { get => eletKor; set => eletKor = value; }
         public string Nem { get => nem; set => nem = value; }
         public ILakohaz LakohazID { get => lakohazID; set => lakohazID = value; }
-        public string Munka { get => munka; set => munka = value; }
-        public List<string> Tulajdon { get => tulajdon; set => tulajdon = value; }
-        public int Penz { get => penz; set => penz = value; }
-        public string Statusz { get => statusz; set => statusz = value; }
-        public int BoldogsagSzint { get => boldogsagSzint; set => boldogsagSzint = value; }
-        public int EhessegSzint { get => ehessegSzint; set => ehessegSzint = value; }
-        public int SzomjSzint { get => szomjSzint; set => szomjSzint = value; }
-        public int Fizikum { get => fizikum; set => fizikum = value; }
         public bool HazasE { get => hazasE; set => hazasE = value; }
+        public Ember Hazastars { get => hazastars; set => hazastars = value; }
         public bool VarandosE { get => varandosE; set => varandosE = value; }
-        public int EgeszsegSzint { get => egeszsegSzint; set => egeszsegSzint = value; }
-        public int VeralkoholSzint { get => veralkoholSzint; set => veralkoholSzint = value; }
+        public int VarandossagSzamlalo { get => varandossagSzamlalo; set => varandossagSzamlalo = value; }
+        public bool HalottE { get => halottE; set => halottE = value; }
         internal Bankszamla Bankszamla { get => bankszamla; set => bankszamla = value; }
+
+        public void Halal()
+        {
+            this.halottE = true;
+        }
+        public void VarandossagKapcsolo()
+        {
+            this.varandosE = !this.varandosE;
+        }
+        public void HazassagKotes(Ember leendoHazastars)
+        {
+            this.hazasE = true;
+            this.hazastars = leendoHazastars;
+        }
+        public void Elvalas()
+        {
+            this.hazasE = false;
+            this.hazastars = null;
+        }
+        public void Hetente()
+        {
+            Random rnd = new Random();
+            int halalozasiSzamAlap = rnd.Next(1, 1001);
+            int halalozasiSzam80 = rnd.Next(1, 101);
+            this.eletKor += 0.02;
+            if (this.eletKor < 80)
+            {
+                if (halalozasiSzamAlap == 67)
+                {
+                    Halal();
+                    return;
+                }
+            }
+            else
+            {
+                if (halalozasiSzam80 == 67)
+                {
+                    Halal();
+                    return;
+                }
+            }
+            if (this.varandosE)
+            {
+                this.varandossagSzamlalo += 1;
+            }
+            if (varandossagSzamlalo >= 39)
+            {
+                VarandossagKapcsolo();
+                this.varandossagSzamlalo = 0;
+            }
+        }
     }
-    
-    // TODO boldogsag meg ilyenek bentrol valtoztatni (mint egy metodus)
 }
